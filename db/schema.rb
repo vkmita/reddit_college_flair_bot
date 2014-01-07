c82279cba9692382eef41ff92054ca493f7462c1
Victor Kmita <polishsurfman@gmail.com>
2014-01-06 23:49:07 -0800

    First migration

---
+++ db/schema.rb
@@ -0,0 +1,34 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20140107072319) do
+
+  create_table "subreddits", force: true do |t|
+    t.integer  "moderator_id"
+    t.integer  "user_id"
+    t.string   "name",         limit: 20
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "subreddits", ["moderator_id"], name: "index_subreddits_on_moderator_id"
+  add_index "subreddits", ["user_id"], name: "index_subreddits_on_user_id"
+
+  create_table "users", force: true do |t|
+    t.string   "username",      limit: 20
+    t.string   "password_hash"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
