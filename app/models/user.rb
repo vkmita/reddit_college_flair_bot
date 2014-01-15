21cbdb0dfa34c8059cbb31eea8b6a6d6473e58eb
Victor Kmita <polishsurfman@gmail.com>
2014-01-14 18:50:26 -0800

    Added has_many :authentications to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   validates_presence_of :username
   validates_length_of :username, :maximum => 20
 
-  has_many :subreddits
+  has_many :subreddits, :authentications
+
 
   devise :confirmable
 end
