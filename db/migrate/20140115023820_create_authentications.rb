a17c5f1508e3d153ee60fbb96dca0de6f271d9a5
Victor Kmita <polishsurfman@gmail.com>
2014-01-14 18:41:21 -0800

    Make authentications migration valid for development

--- db/migrate/20140115023820_create_authentications.rb
+++ db/migrate/20140115023820_create_authentications.rb
@@ -1,13 +1,11 @@
 class CreateAuthentications < ActiveRecord::Migration
   def change
-    if Rails.env.production?
-      create_table :authentications do |t|
-        t.integer  "user_id"
-        t.string   "provider"
-        t.string   "uid"
-        t.datetime "created_at"
-        t.datetime "updated_at"
-      end
+    create_table :authentications do |t|
+      t.integer  "user_id"
+      t.string   "provider"
+      t.string   "uid"
+      t.datetime "created_at"
+      t.datetime "updated_at"
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140107075059) do
+ActiveRecord::Schema.define(version: 20140115023820) do
 
   create_table "subreddits", force: true do |t|
     t.integer  "moderator_id"
