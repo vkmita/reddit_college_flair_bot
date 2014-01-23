5f92d57d3f4e39942ad192e4183b32d2fa3fe913
Victor Kmita <polishsurfman@gmail.com>
2014-01-22 23:59:01 -0800

    redirect to subreddits index when authenticated

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,30 +1,31 @@
 class AuthenticationsController < ApplicationController
 
   def create
-    authentication = Authentication.new({ :provider => omniauth_provider, :uid => omniauth_uid })
+    @authentication = Authentication.new({ :provider => omniauth_provider, :uid => omniauth_uid })
 
     flash[:notice] = I18n.t('controller.authentication.login.successful', :username => current_user.username)
-    render 'users/new'
 
-    if authentication
-      login(authentication.user)
+    render 'subreddits/index'
 
-      redirect_to subreddits_path
-    elsif current_user
-      current_user.authentications.create(:provider => omniauth['provider'], :uid => omniauth['uid'])
-      flash[:notice] = 'Authentication successful.'
-      redirect_to authentications_url
-    else
-      user = User.new
-      user.apply_omniauth(omniauth)
-      if user.save
-        flash[:notice] = 'Signed in successfully.'
-        sign_in_and_redirect(:user, user)
-      else
-        session[:omniauth] = omniauth.except('extra')
-        redirect_to authentications_url
-      end
-    end
+    #if authentication
+    #  login(authentication.user)
+    #
+    #  redirect_to subreddits_path
+    #elsif current_user
+    #  current_user.authentications.create(:provider => omniauth['provider'], :uid => omniauth['uid'])
+    #  flash[:notice] = 'Authentication successful.'
+    #  redirect_to authentications_url
+    #else
+    #  user = User.new
+    #  user.apply_omniauth(omniauth)
+    #  if user.save
+    #    flash[:notice] = 'Signed in successfully.'
+    #    sign_in_and_redirect(:user, user)
+    #  else
+    #    session[:omniauth] = omniauth.except('extra')
+    #    redirect_to authentications_url
+    #  end
+    #end
   end
 
   def destroy
