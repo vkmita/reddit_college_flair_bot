bb8e45a322ce3a8e5ca4a7b0d55a5419b13d969f
Victor Kmita <polishsurfman@gmail.com>
2014-01-22 23:41:35 -0800

    subreddit validates

--- app/models/subreddit.rb
+++ app/models/subreddit.rb
@@ -1,13 +1,13 @@
 class Subreddit < ActiveRecord::Base
   validates_presence_of :moderator, :name
-  validates :moderator_can_moderate
+  validate :validate_moderator_can_moderate
 
   has_many :moderator, :class_name => "Moderator"
   has_many :users
 
   private
 
-  def moderator_can_moderate
+  def validate_moderator_can_moderate
     moderator.can_moderate?(name)
   end
 end
