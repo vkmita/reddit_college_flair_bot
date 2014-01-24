a86540dbddcba95bbe023f7f15116c92c7a70c69
Victor Kmita <polishsurfman@gmail.com>
2014-01-23 22:42:46 -0800

    Try class eval

--- lib/reddit_college_flair_bot/extensions/omniauth_reddit.rb
+++ lib/reddit_college_flair_bot/extensions/omniauth_reddit.rb
@@ -1,24 +1,22 @@
-module OmniAuth
-  module Strategies
-    class Reddit < OmniAuth::Strategies::OAuth2
-      extra do
-        {
-          'username' => raw_info['name'],
-          'subreddits' => { 'subscriber' => subscriber_subreddits, 'moderator' => moderator_subreddits }
-        }
-      end
-
-      def username
-        @username ||= raw_info['name']
-      end
-
-      def subscriber_subreddits
-        @subscriber_subreddits ||= access_token.get('/subreddits/mine/subscriber').parsed || {}
-      end
-
-      def moderator_subreddits
-        @moderator_subreddits ||= access_token.get('/subreddits/mine/moderator').parsed || {}
-      end
-    end
+module OmniAuth:: Strategies::Reddit.class_eval do
+  extra do
+    {
+        'username' => raw_info['name'],
+        'subreddits' => { 'subscriber' => subscriber_subreddits, 'moderator' => moderator_subreddits }
+    }
   end
+
+  def username
+    @username ||= raw_info['name']
+  end
+
+  def subscriber_subreddits
+    @subscriber_subreddits ||= access_token.get('/subreddits/mine/subscriber').parsed || {}
+  end
+
+  def moderator_subreddits
+    @moderator_subreddits ||= access_token.get('/subreddits/mine/moderator').parsed || {}
+  end
+end
+
 end
